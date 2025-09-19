8912c2e98636e02fc299b873149a03031c29dc1f
Matheus Dev <matheushnt.dev@gmail.com>
2025-09-19 11:26:20 -0300

    Fundamentos do Prisma ORM

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import fastify from 'fastify';
+import { PrismaClient } from './generated/client';
 
 export const app = fastify();
+
+const prisma = new PrismaClient();
+
+prisma.user.create({
+  data: {
+    name: 'Matheus Henrique',
+    email: 'matheus@example.com.br',
+  },
+});
